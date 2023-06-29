f6d99273e6657af1e08711549b02f67b344bafa6
ds58 <30220598+ds58@users.noreply.github.com>
2023-06-29 10:35:44 -0500

    Start ZED preset

---
+++ zed/cppbuild.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# This file is meant to be included by the parent cppbuild.sh script
+if [[ -z "$PLATFORM" ]]; then
+    pushd ..
+    bash cppbuild.sh "$@" zed
+    popd
+    exit
+fi
+
+# Requires libusb-dev, CUDA
+
+ZED_C_VERSION=3.8
+download https://github.com/stereolabs/zed-c-api/archive/refs/tags/v$ZED_C_VERSION.tar.gz zed-c-api-$ZED_C_VERSION.tar.gz
+mkdir -p $PLATFORM
+cd $PLATFORM
+INSTALL_PATH=`pwd`
+tar -xzvf ../zed-c-api-$ZED_C_VERSION.tar.gz
+cd zed-c-api-$ZED_C_VERSION
+
+case $PLATFORM in
+    linux-x86*)
+        if [[ ! -d "/usr/local/zed" ]]; then
+            echo "Please install ZED under the default installation directory: /usr/local/zed"
+            exit 1
+        fi
+        mkdir build && cd build
+        cmake .. -DCMAKE_BUILD_TYPE=Release
+        make
+        # Installs to /usr/local/zed
+        # Include: /usr/local/zed/include/sl/c_api/
+        # Lib: /usr/local/zed/lib/libsl_zed_c.so
+        make install
+        ;;
+    *)
+        echo "Error: Platform \"$PLATFORM\" is not supported"
+        ;;
+esac
---
+++ zed/src/main/java/org/bytedeco/zed/presets/zed.java
@@ -0,0 +1,24 @@
+package org.bytedeco.zed.presets;
+
+import org.bytedeco.javacpp.*;
+import org.bytedeco.javacpp.annotation.*;
+import org.bytedeco.javacpp.tools.*;
+
+@Properties(
+    value = {
+        @Platform(include = {"/usr/local/zed/include/sl/c_api/types_c.h",
+                             "/usr/local/zed/include/sl/c_api/zed_interface.h"},
+              includepath = "/usr/local/cuda-11.7/targets/x86_64-linux/include",
+              linkpath = "/usr/local/zed/lib",
+              link = "sl_zed_c")
+    },
+    target = "org.bytedeco.zed",
+    global = "org.bytedeco.zed.global.zed"
+)
+public class zed implements InfoMapper {
+    static { Loader.checkVersion("org.bytedeco", "zed"); }
+
+    public void map(InfoMap infoMap) {
+        infoMap.put(new Info("CUcontext", "cudaStream_t").skip());
+    }
+}
